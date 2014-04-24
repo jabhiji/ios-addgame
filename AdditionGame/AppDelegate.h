4b3ad4249719ca5a76add87f345c01cefb34003e
Abhijit Joshi <joshi1974@gmail.com>
2014-04-24 16:50:21 -0400

    Initial Commit

---
+++ AdditionGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AdditionGame
+//
+//  Created by Abhijit Joshi on 4/24/14.
+//  Copyright (c) 2014 XYZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AdditionGame/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AdditionGame
+//
+//  Created by Abhijit Joshi on 4/24/14.
+//  Copyright (c) 2014 XYZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
