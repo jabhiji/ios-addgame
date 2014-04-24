f6858a33d18d50cda87e25df31c7595577fa506f
Abhijit Joshi <joshi1974@gmail.com>
2014-04-24 18:17:44 -0400

    Basic working version with console output instead of alerts…

--- AdditionGame/ViewController.m
+++ AdditionGame/ViewController.m
@@ -12,27 +12,39 @@
 @property (retain, nonatomic) IBOutlet UILabel *label;
 @property (retain, nonatomic) IBOutlet UITextField *answer;
 - (IBAction)submit:(id)sender;
-
+- (void) generate;
 @end
 
 @implementation ViewController
 
+@synthesize label;
+@synthesize answer;
+
 - (void)viewDidLoad
 {
-    [self setLabel:nil];
-    [self setAnswer:nil];
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    [self generate];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    [self setLabel:nil];
+    [self setAnswer:nil];
 }
 
 - (IBAction)submit:(id)sender {
     NSLog(@"You clicked submit");
+    int num = [answer.text intValue];
+    if (num == label.tag) {
+        NSLog(@"CORRECT");
+        [self generate];
+    } else {
+        NSLog(@"WRONG! TRY AGAIN");
+    }
 }
 
 - (void) generate
@@ -40,8 +52,8 @@
     int a = 1 + arc4random()%9;
     int b = 1 + arc4random()%9;
     int sum = a + b;
-    _label.text = [NSString stringWithFormat:@"%d + %d = ",a,b];
-    _label.tag = sum;
+    label.text = [NSString stringWithFormat:@"%d + %d = ",a,b];
+    label.tag = sum;
 }
 
 @end
